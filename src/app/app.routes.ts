03ce57b1d82e779309d19cba522351b5cc0e91b4
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2025-02-21 13:04:13 +0100

    Creando estructura de proyecto visual

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { TimerComponent } from './timer/timer.component';
+import { MatchComponent } from './match/match.component';
+import { OptionsComponent } from './options/options.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'main-menu', pathMatch: 'full' },
+  { path: 'main-menu', component: MainMenuComponent },
+  { path: 'timer', component: TimerComponent },
+  { path: 'match', component: MatchComponent },
+  { path: 'options', component: OptionsComponent },
+  { path: '**', redirectTo: 'main-menu' }
+];
---
+++ src/app/match/match.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-match',
+  imports: [],
+  templateUrl: './match.component.html',
+  styleUrl: './match.component.scss'
+})
+export class MatchComponent {
+
+}
